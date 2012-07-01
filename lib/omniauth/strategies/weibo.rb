756bd6b0e82d7b8b01516b43aaa3b58b9ba42a14
Shi Chuan <shichuanr@gmail.com>
2012-07-01 14:01:19 +0100

    added image path

--- lib/omniauth/strategies/weibo.rb
+++ lib/omniauth/strategies/weibo.rb
@@ -30,7 +30,7 @@ module OmniAuth
           :nickname => raw_info['screen_name'],
           :name => raw_info['name'],
           :location => raw_info['location'],
-          :image => raw_info['profileImageUrl'],
+          :image => raw_info['profile_image_url'],
           :description => raw_info['description'],
           :urls => {
             'Website' => raw_info['url'],
