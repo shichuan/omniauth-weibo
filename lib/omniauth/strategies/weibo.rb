becc1047125d03a005f0f7727708b5bf93502070
Shi Chuan <shichuanr@gmail.com>
2012-07-01 13:18:24 +0100

    changed image path name

--- lib/omniauth/strategies/weibo.rb
+++ lib/omniauth/strategies/weibo.rb
@@ -30,7 +30,7 @@ module OmniAuth
           :nickname => raw_info['screen_name'],
           :name => raw_info['name'],
           :location => raw_info['location'],
-          :image => raw_info['profile_image_url'],
+          :image => raw_info['profileImageUrl'],
           :description => raw_info['description'],
           :urls => {
             'Website' => raw_info['url'],
